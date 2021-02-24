ee95794b1fd58a0d7deb6e929b6c589fcdcbe890
Rakesh Gunnam <46306449+RAKESHGUNNAM@users.noreply.github.com>
2021-02-24 15:07:18 +0530

    Full Project files

---
+++ FolderRead.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace FilterImg
+{
+    partial class FolderRead
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FolderRead));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.FolderPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Prev = new System.Windows.Forms.Button();
+            this.Next = new System.Windows.Forms.Button();
+            this.Select = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // FolderPath
+            // 
+            this.FolderPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FolderPath.Location = new System.Drawing.Point(12, 12);
+            this.FolderPath.Name = "FolderPath";
+            this.FolderPath.Size = new System.Drawing.Size(342, 22);
+            this.FolderPath.TabIndex = 0;
+            this.FolderPath.TextChanged += new System.EventHandler(this.FolderPath_TextChanged);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(360, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 22);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(540, 459);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(561, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(211, 459);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Prev
+            // 
+            this.Prev.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Prev.Location = new System.Drawing.Point(592, 12);
+            this.Prev.Name = "Prev";
+            this.Prev.Size = new System.Drawing.Size(75, 40);
+            this.Prev.TabIndex = 4;
+            this.Prev.Text = "<<";
+            this.Prev.UseVisualStyleBackColor = true;
+            this.Prev.Click += new System.EventHandler(this.Prev_Click);
+            // 
+            // Next
+            // 
+            this.Next.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Next.Location = new System.Drawing.Point(684, 12);
+            this.Next.Name = "Next";
+            this.Next.Size = new System.Drawing.Size(75, 40);
+            this.Next.TabIndex = 5;
+            this.Next.Text = ">>";
+            this.Next.UseVisualStyleBackColor = true;
+            this.Next.Click += new System.EventHandler(this.Next_Click);
+            this.Next.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Next_KeyDown);
+            this.Next.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Next_KeyPress);
+            this.Next.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Next_KeyUp);
+            // 
+            // Select
+            // 
+            this.Select.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Select.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Select.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Select.Location = new System.Drawing.Point(467, 12);
+            this.Select.Name = "Select";
+            this.Select.Size = new System.Drawing.Size(85, 40);
+            this.Select.TabIndex = 6;
+            this.Select.Text = "Select";
+            this.Select.UseVisualStyleBackColor = false;
+            this.Select.Click += new System.EventHandler(this.Select_Click);
+            // 
+            // FolderRead
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.Select);
+            this.Controls.Add(this.Next);
+            this.Controls.Add(this.Prev);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.FolderPath);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FolderRead";
+            this.Text = "Filter IMG";
+            this.Load += new System.EventHandler(this.FolderRead_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FolderRead_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox FolderPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Prev;
+        private System.Windows.Forms.Button Next;
+        private System.Windows.Forms.Button Select;
+    }
+}
+
---
+++ FolderRead.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FilterImg
+{
+    public partial class FolderRead : Form
+    {
+        public FolderRead()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+        private void FolderRead_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                FolderPath.Text = folderBrowserDialog1.SelectedPath;
+                
+                listBox1.Items.Clear();
+                string[] imgFiles = Directory.GetFiles(FolderPath.Text, "*jpg", SearchOption.TopDirectoryOnly);
+
+                foreach (string jpgFile in imgFiles)
+                {
+                    listBox1.Items.Add(jpgFile);
+                }
+                //for (int i = 0; i < imgFiles.Length; i++)
+                //{
+                //    listBox1.Items.Add(imgFiles[i]);
+                //}
+                if (listBox1.Items.Count > 0)
+                {
+                    listBox1.SetSelected(0, true);
+                    pictureBox1.Image = new Bitmap(listBox1.SelectedItem.ToString());
+                }
+                else
+                {
+                    MessageBox.Show("JPG Files Not Available");
+                }
+            }
+        }
+
+        private void FolderPath_TextChanged(object sender, EventArgs e)
+        {
+            string Path = FolderPath.Text;
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                // Get the currently selected item in the ListBox.
+                pictureBox1.Image = new Bitmap(listBox1.SelectedItem.ToString());
+            }
+        }
+
+        private void Next_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex+1 < listBox1.Items.Count)
+            {
+                listBox1.SetSelected(listBox1.SelectedIndex + 1, true);
+            }
+            //else
+            //{
+               
+            //    Next.Enabled = false;
+            //}
+        }
+
+        private void Prev_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex - 1 >= 0)
+            {
+                listBox1.SetSelected(listBox1.SelectedIndex - 1, true);
+            }
+        }
+
+        private void Select_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                string destPath = FolderPath.Text + @"\SelectedItems\";
+                if (Directory.Exists(destPath))
+                {
+                    File.Copy(listBox1.SelectedItem.ToString(), $"{ destPath}{ Path.GetFileName(listBox1.SelectedItem.ToString())}");
+
+                    if (listBox1.Items.Count == 1)
+                    {
+                        listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                    }
+
+                    if (listBox1.SelectedIndex + 1 < listBox1.Items.Count)
+                    {
+                        listBox1.SetSelected(listBox1.SelectedIndex + 1, true);
+
+                        listBox1.Items.RemoveAt(listBox1.SelectedIndex - 1);
+                    }
+                    else if(listBox1.SelectedIndex-1 < listBox1.Items.Count && listBox1.SelectedIndex-1 >= 0)
+                    {
+                       
+                        listBox1.SetSelected(listBox1.SelectedIndex - 1, true);
+
+                        listBox1.Items.RemoveAt(listBox1.SelectedIndex + 1);
+                    }
+                }
+                else
+                {
+                    Directory.CreateDirectory(FolderPath.Text + @"\SelectedItems");
+                    File.Copy(listBox1.SelectedItem.ToString(), $"{ destPath}{ Path.GetFileName(listBox1.SelectedItem.ToString())}");
+
+                    if (listBox1.SelectedIndex + 1 < listBox1.Items.Count)
+                    {
+                        listBox1.SetSelected(listBox1.SelectedIndex + 1, true);
+
+                        listBox1.Items.RemoveAt(listBox1.SelectedIndex - 1);
+                    }
+                    else
+                    {
+                        listBox1.SetSelected(listBox1.SelectedIndex - 1, true);
+
+                        listBox1.Items.RemoveAt(listBox1.SelectedIndex + 1);
+                    }
+                }
+            }
+        }
+
+        private void Next_KeyDown(object sender, KeyEventArgs e)
+        {
+            //MessageBox.Show(e.KeyCode.ToString());
+            //if(e.KeyCode==Keys.Enter)
+            //{
+            //    MessageBox.Show("D Pressed");
+            //    //if (listBox1.SelectedIndex + 1 < listBox1.Items.Count)
+            //    //{
+            //    //    listBox1.SetSelected(listBox1.SelectedIndex + 1, true);
+            //    //}
+            //}
+        }
+
+        private void Next_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Next_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //MessageBox.Show(e.KeyChar.ToString());
+            //if(e.KeyChar==(char)Keys.A)
+            //{
+            //    if (listBox1.SelectedIndex - 1 >= 0)
+            //    {
+            //        listBox1.SetSelected(listBox1.SelectedIndex - 1, true);
+            //    }
+            //}
+        }
+
+        private void FolderRead_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.D)
+            {
+                //MessageBox.Show("A");
+                Next.PerformClick();
+            }
+            if(e.KeyCode==Keys.A)
+            {
+                Prev.PerformClick();
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                Select.PerformClick();
+            }
+        }
+    }
+}
